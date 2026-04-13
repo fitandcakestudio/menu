9c96d1d2bf1a0b0cccc91372b6a108b871ebc60f
Fit and Cake Studio <fitandcakestudio@gmail.com>
2026-04-13 13:09:08 +0300

    file architecture updated

---
+++ js/admin.js
@@ -0,0 +1,21 @@
+const supabaseClient = supabase.createClient(
+  "https://zjqormxrjvhvbmdfazej.supabase.co",
+  "sb_publishable_OLNn50ApEwEeLZQk49u74A_Eue6jUfG"
+);
+
+async function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { data, error } = await supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    console.log(error);
+    document.getElementById("error").innerText = error.message;
+  } else {
+    window.location.href = "adminpanel.html";
+  }
+}
